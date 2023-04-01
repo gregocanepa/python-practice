15bdcab491dd6cb1b590100d2fa13997c5237fff
gregorio <gregorio.canepa94@gmail.com>
2023-04-01 14:04:38 -0300

    more activities with strings

--- string_exercises.py
+++ string_exercises.py
@@ -12,3 +12,27 @@
 # 5) write a function that takes two parameter: first parameter: a random sentence, second parameter: a single word.
 #    The function should return true if the word in the second parameter exists in the sentence from the first parameter.
 #    Example: "I drink mate all the time." "coffee". Output: False
+
+
+# 6) Write a Python program to check whether a string starts with specified characters. 
+# Example: main_string = "Java is the worst." substring = "Python".
+# Expected output: False
+
+# 7) Write a Python function that takes a list of words and return the longest word and the length of the longest one.
+# Sample list: ["this", "exercise", "is", "quite", "easy"] 
+# Expected output: 'Longest word: "exercise". Length of the longest word: 9'
+
+# 8) Exercise 5 2.0: Write a function that takes two parameters: 
+# first parameter: a random sentence, second parameter: a single word.
+# The function should return true if the word in the second parameter exists in the sentence from the first parameter.
+# You can't use "if word in sentence" sintax.
+# Example: "I drink mate all the time." "coffee". Output: False.
+
+# 9) Write a Python program to count occurrences of a substring in a string.
+# main_string = "Python is a great language. Python can be used for web development and data science.
+#               It's name comes from the british comedy group Monty Python."
+# substring = "Python"
+
+# 10) Write a python program that replaces commas "," with dots "." and dots with commas.
+# original_string = "In some countries people write decimals like this: 3.14, whereas other write it this way 3,14."
+# expected output: "In some countries people write decimals like this: 3,14. whereas other write it this way 3.14,"
